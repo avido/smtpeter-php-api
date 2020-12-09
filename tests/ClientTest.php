b945335c4eb78ab731d4297a779c635478eb6892
Sebastiaan <sebastiaan@avido.nl>
2020-12-09 15:31:48 +0100

    Base test

---
+++ tests/ClientTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Avido\Smtpeter\Tests;
+
+use Avido\Smtpeter\Exceptions\SmtpeterException;
+
+class ClientTest extends TestCase
+{
+    /** @testx */
+    public function client_has_templates_endpoint()
+    {
+        $this->assertInstanceOf(ServicePoints::class, $this->client->servicePoints);
+    }
+
+    /** @test */
+    public function performing_an_http_call_without_setting_an_api_token_throws_an_exception()
+    {
+        $this->expectException(SmtpeterException::class);
+        $this->client->setApiToken(null);
+        $this->client->performHttpCall('GET', 'some-resource');
+    }
+}
